2905d7649d9e9bcdd3d6df0ed9385cf5f8a68586
Arnaud 'Bluexin' Solé <bluexin.games@gmail.com>
2020-01-05 13:05:03 +0100

    Add the Aeron Wrappers previously in Brahma

---
+++ readme.md
@@ -0,0 +1,3 @@
+# KAeron
+
+[Kotlin Coroutines](https://github.com/Kotlin/kotlinx.coroutines) extensions for [Aeron](https://github.com/real-logic/aeron).
---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "kaeron"
+
+val kotlin_version: String by settings
+
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.namespace) {
+                "org.jetbrains.kotlin" -> useVersion(kotlin_version)
+                "org.jetbrains.kotlin.plugin" -> useVersion(kotlin_version)
+            }
+        }
+    }
+}
